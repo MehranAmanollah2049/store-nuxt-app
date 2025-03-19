1fb3ce8bb43d07ebc910316ab929b07a69008646
Mehran <mehranamanollah13@gmail.com>
2025-03-19 12:35:08 +0330

    bug fixed

--- components/Product/ProductCardLoading.vue
+++ components/Product/ProductCardLoading.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div class="w-full h-[390px] flex items-start justify-center flex-col gap-3 border-4 border-white px-1">
-        <div class="w-full h-[220px] rounded-lg bg-gray-200 animate-pulse"></div>
+        <div class="w-full h-[220px] rounded-lg bg-gray-200 animate-pulse max-[470px]:h-[145px]"></div>
         <div class="w-full h-[10px] rounded-full bg-gray-200 animate-pulse mt-2"></div>
         <div class="w-[50%] h-[10px] rounded-full bg-gray-200 animate-pulse"></div>
 
