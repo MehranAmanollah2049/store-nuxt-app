7cedf5a681a311434372c25cb344d73d01a59913
Mehran <mehranamanollah13@gmail.com>
2025-03-19 11:53:58 +0330

    footer text changed

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="w-full center py-8 pt-10 mt-auto max-[800px]:pb-[90px]">
         <div class="w-custom center">
-            <p class="text-title font-medium"> Made with ❤️ by <span class="text-theme font-medium"> Mehran amanollah </span></p>
+            <p class="text-title font-medium"> Made by <span class="text-theme font-medium"> Mehran amanollah </span></p>
         </div>
     </div>
 </template>
