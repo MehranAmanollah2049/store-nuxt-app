ff0bfc029bbec9a5db784f94cc72454405c787b3
Mehran <mehranamanollah13@gmail.com>
2025-03-24 00:19:50 +0330

    bug fixe

--- components/Product-single/StarRating.vue
+++ components/Product-single/StarRating.vue
@@ -65,9 +65,7 @@ const login_alert = () => {
     <div class="w-full flex items-center justify-between my-7 p-5 bg-gray-100/60 rounded-xl">
         <p class="font-medium text-title text-[16px]">امتیاز خود را ثبت کنید: </p>
         <template v-if="!loading">
-            <div class="center translate-y-[3px]" @click="login_alert" :class="{
-                'pointer-events-none': !userStore.isLoggedIn()
-            }" :style="{ direction: 'ltr' }">
+            <div class="center translate-y-[3px]" :style="{ direction: 'ltr' }">
                 <NuxtRating :read-only="false" border-color="#f54900" active-color="#f54900" inactive-color="#fff"
                     :rating-step="0.5" :rounded-corners="true" :border-width="6" :rating-size="12" :rating-spacing="3"
                     :rating-value="model.rate" @rating-selected="selectRate" />
